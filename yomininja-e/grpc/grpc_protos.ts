ac7ada900998a48cca98d4689cab76b6a5ceda6e
MatheusM8 <matheusmoura8@hotmail.com>
2023-09-18 01:35:25 -0300

    Added missing grpc proto loader option

--- yomininja-e/grpc/grpc_protos.ts
+++ yomininja-e/grpc/grpc_protos.ts
@@ -3,7 +3,9 @@ import * as protoLoader from '@grpc/proto-loader';
 import { join } from "path";
 import { ProtoGrpcType } from './rpc/ocr_service';
 
-const ocrServicePackageDefinition = protoLoader.loadSync( join(__dirname, "../grpc/protos/ocr_service.proto") );
+const ocrServicePackageDefinition = protoLoader.loadSync( join(__dirname, "../grpc/protos/ocr_service.proto"), {
+    keepCase: true,
+});
 
 export const ocrServiceProto = grpc.loadPackageDefinition(
     ocrServicePackageDefinition
