a527173459f0260ad542408dc4abe17a41797d3e
MatheusM8 <matheusmoura8@hotmail.com>
2023-09-19 18:51:30 -0300

    Updated react and installed MUI material packages

--- yomininja-e/renderer/pages/ocr-overlay.tsx
+++ yomininja-e/renderer/pages/ocr-overlay.tsx
@@ -1,17 +1,33 @@
 import { useEffect, useState } from 'react';
-import Layout from '../components/Layout';
 import { OcrResult } from '../../electron-src/@core/domain/ocr_result/ocr_result';
+import { styled } from '@mui/material/styles';
+
+
+const OverlayFrame = styled('div')({
+  border: 'solid 2px red',
+  height: '99.5vh',
+  overflow: 'hidden',
+});
+
+const OcrItem = styled('div')({
+  border: 'solid 1px red',
+  borderRadius: '10px',  
+});
 
 export default function OcrOverlayPage() {
 
   const [ ocrResult, setOcrResult ] = useState< OcrResult >();
 
   useEffect( () => {
+    document.body.style.margin = "0";
+    document.body.style.padding = "0";
+    document.body.style.overflow = "hidden";
+    
 
     function handleOcrResult( _event, args: OcrResult ) {
       console.log( args );
       setOcrResult( args )
-    } 
+    }
     // add a listener to 'message' channel
     global.ipcRenderer.addListener('message', handleOcrResult)
 
@@ -23,13 +39,11 @@ export default function OcrOverlayPage() {
   }, []);
   
 
-  return (
-    <Layout title="Ocr Overlay">
-
+  return (    
+    <OverlayFrame>
       { ocrResult?.results?.map( ( item, idx ) => {
-        return <div key={idx}> { item.text } </div>
-      }) }
-      
-    </Layout>
+        return <OcrItem key={idx}> { item.text } </OcrItem>
+      }) }      
+    </OverlayFrame>
   );
 }
