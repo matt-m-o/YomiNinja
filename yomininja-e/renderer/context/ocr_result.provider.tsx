91b45befbf982e19362b2860566ccc0fafe3b8d8
MatheusM8 <matheusmoura8@hotmail.com>
2023-09-20 01:26:15 -0300

    Implemented ocr item box ui positioning

--- yomininja-e/renderer/context/ocr_result.provider.tsx
+++ yomininja-e/renderer/context/ocr_result.provider.tsx
@@ -1,37 +1,94 @@
 import { PropsWithChildren, createContext, useEffect, useState } from "react";
-import { OcrResult } from "../../electron-src/@core/domain/ocr_result/ocr_result";
+import { OcrItem, OcrResult, OcrResultContextResolution } from "../../electron-src/@core/domain/ocr_result/ocr_result";
 
+
+export type OcrResultBoxPositionPcts = {
+    top: number;
+    left: number;
+};
+
+export type OcrResultBoxDimensionsPcts = {
+    width: number;
+    height: number;
+};
+
+// Ocr result box percentages to position, scale and rotate the boxes
+export type OcrResultBoxUI = {
+    box_position: OcrResultBoxPositionPcts;
+    dimensions?: OcrResultBoxDimensionsPcts;    
+    angle_degrees?: number;
+};
+
+export interface OcrItemUI {
+    text: string;
+    box_ui: OcrResultBoxUI;
+};
 
 export type OcrResultContextType = {
-    ocrResult: OcrResult;    
+    ocrResult: OcrResult;
+    ocrUIitems: OcrItemUI[];
 };
+
+function calculateOcrItemBoxPosition( ocrResultItem: OcrItem, contextResolution: OcrResultContextResolution ): OcrResultBoxPositionPcts {
+
+    const { width, height } = contextResolution;    
+
+    const topLeftXPixels = ocrResultItem.box.top_left.x;
+    const position_left = ( 1 - ( ( width - topLeftXPixels ) / width ) ) * 100;
+
+    const topLeftYPixels = ocrResultItem.box.top_left.y;
+    const position_top = ( 1 - ( ( height - topLeftYPixels ) / height ) ) * 100;
+    
+    return {
+        left: position_left,
+        top: position_top
+    }
+}
 
 export const OcrResultContext = createContext( {} as OcrResultContextType );
 
 
-
 export const OcrResultProvider = ( { children }: PropsWithChildren ) => {
 
-    const [ ocrResult, setOcrResult ] = useState< OcrResult >();  
+    const [ ocrResult, setOcrResult ] = useState< OcrResult >();
+    const [ ocrUIitems, setOcrUIitems ] = useState< OcrItemUI[] >( [] );
+
   
   
     function ocrResultHandler ( _event, data: OcrResult ) {
         // console.log( data );
         setOcrResult( data );
+
+        data.results.forEach( item => {            
+
+            setOcrUIitems([
+                ...ocrUIitems,
+                {
+                    box_ui: {
+                        box_position: calculateOcrItemBoxPosition( item, data.context_resolution )
+                    },
+                    text: item.text,
+                }
+            ])
+        });
+
+        console.log(ocrUIitems);
+
     }
     
     useEffect( () => {
         global.ipcRenderer.on( 'ocr:result', ocrResultHandler );
-    }, [global.ipcRenderer] );
+    }, [ global.ipcRenderer ] );
     
     
     return (
-      <OcrResultContext.Provider
-        value={{
-          ocrResult
-        }}
-      >
-        {children}
-      </OcrResultContext.Provider>
+        <OcrResultContext.Provider
+            value={{
+                ocrResult,
+                ocrUIitems,
+            }}
+        >
+            {children}
+        </OcrResultContext.Provider>
     );
-  }
+}
