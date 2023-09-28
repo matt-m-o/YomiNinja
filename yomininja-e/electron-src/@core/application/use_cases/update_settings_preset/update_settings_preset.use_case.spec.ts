b184d4c9d38bd1e8ec94caddc9430d64c85e6a32
MatheusM8 <matheusmoura8@hotmail.com>
2023-09-28 00:13:57 -0300

    Implemented  UpdateSettingsPresetUseCase

---
+++ yomininja-e/electron-src/@core/application/use_cases/update_settings_preset/update_settings_preset.use_case.spec.ts
@@ -0,0 +1,57 @@
+import { SettingsPreset } from "../../../domain/settings_preset/settings_preset";
+import { SettingsPresetTypeOrmSchema } from "../../../infra/db/typeorm/settings_preset/settings_preset.schema";
+import SettingsPresetTypeOrmRepository from "../../../infra/db/typeorm/settings_preset/settings_preset.typeorm.repository";
+import { DataSource } from 'typeorm';
+import { UpdateSettingsPresetUseCase, UpdateSettingsPreset_Input } from "./update_settings_preset.use_case";
+
+describe("UpdateSettingsPresetUseCase tests", () => {
+    
+    let defaultPreset: SettingsPreset;
+        
+    let updateSettingsPresetUseCase: UpdateSettingsPresetUseCase;
+
+    let settingsPresetRepo: SettingsPresetTypeOrmRepository;
+
+
+    beforeEach( async () => {
+
+        const dataSource = new DataSource({
+            type: 'sqlite',
+            database: ':memory:',
+            synchronize: true,
+            logging: false,
+            entities: [                
+                SettingsPresetTypeOrmSchema,                
+            ],
+        });
+
+        await dataSource.initialize();
+
+        settingsPresetRepo = new SettingsPresetTypeOrmRepository(
+            dataSource.getRepository( SettingsPreset )
+        );
+        updateSettingsPresetUseCase = new UpdateSettingsPresetUseCase(
+            settingsPresetRepo
+        );
+            
+        defaultPreset = SettingsPreset.create();
+        await dataSource.getRepository( SettingsPreset ).insert( defaultPreset );
+        
+    });
+
+    it("should update the default settings preset", async () => {        
+
+        const input: UpdateSettingsPreset_Input = defaultPreset.toJson();
+
+        input.name = 'custom 1';
+        input.overlay.hotkeys.ocr = 'Ctrl+D';
+
+        await updateSettingsPresetUseCase.execute( input );   
+        
+        const foundPreset = await settingsPresetRepo.findOne({ id: defaultPreset.id });
+
+        expect( foundPreset?.name ).toStrictEqual( input.name );
+        expect( foundPreset?.overlay.hotkeys.ocr ).toStrictEqual( input.overlay.hotkeys.ocr );
+    });
+    
+});
---
+++ yomininja-e/electron-src/@core/application/use_cases/update_settings_preset/update_settings_preset.use_case.ts
@@ -0,0 +1,25 @@
+import { SettingsPreset, SettingsPresetJson } from "../../../domain/settings_preset/settings_preset";
+import { SettingsPresetRepository } from "../../../domain/settings_preset/settings_preset.repository";
+
+export interface UpdateSettingsPreset_Input extends SettingsPresetJson {
+}
+
+export class UpdateSettingsPresetUseCase {
+
+    constructor(
+        public settingsPresetRepo: SettingsPresetRepository,
+    ) {}
+
+    async execute( input: UpdateSettingsPreset_Input ): Promise< void > {
+
+        const settingsPreset = await this.settingsPresetRepo.findOne({ id: input.id });
+
+        if ( !settingsPreset )
+            return;
+
+        settingsPreset.name = input.name;
+        settingsPreset.updateOverlaySettings(input.overlay);
+
+        await this.settingsPresetRepo.update( settingsPreset );
+    }
+}
