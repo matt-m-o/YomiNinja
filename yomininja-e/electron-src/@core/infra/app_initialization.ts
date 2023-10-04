5ae8c197b9fd5d131f4ecc135d8778daedf0cc87
MatheusM8 <matheusmoura8@hotmail.com>
2023-10-04 17:55:53 -0300

    Implemented function to populate languages repository on app initialization

--- yomininja-e/electron-src/@core/infra/app_initialization.ts
+++ yomininja-e/electron-src/@core/infra/app_initialization.ts
@@ -1,12 +1,29 @@
 import './container_registry/container_registry';
-import { Language } from '../domain/language/language';
+import { Language, Language_CreationInput } from '../domain/language/language';
 import { Profile } from '../domain/profile/profile';
 import { SettingsPreset } from "../domain/settings_preset/settings_preset";
 import { get_MainDataSource } from "./container_registry/db_registry";
 import { get_LanguageRepository, get_ProfileRepository, get_SettingsPresetRepository } from "./container_registry/repositories_registry";
 import os from 'os';
+import LanguageTypeOrmRepository from './db/typeorm/language/language.typeorm.repository';
 
 export let activeProfile: Profile;
+
+
+async function populateLanguagesRepository( languageRepo: LanguageTypeOrmRepository ) {
+    const languages: Language_CreationInput[] = [
+        { name: 'japanese', two_letter_code: 'ja' },
+        { name: 'english', two_letter_code: 'en' },
+    ]
+    for ( const data of languages ) {
+        const exists = await languageRepo.findOne({ two_letter_code: data.two_letter_code });
+
+        if ( exists ) continue;
+
+        await languageRepo.insert( Language.create( data ) )
+            .catch( console.error );
+    }    
+}
 
 export async function initializeApp() {
     
@@ -21,6 +38,8 @@ export async function initializeApp() {
         const settingsPresetRepo = get_SettingsPresetRepository();
         const profileRepo = get_ProfileRepository();
 
+        await populateLanguagesRepository( languageRepo );
+
         let defaultSettingsPreset = await settingsPresetRepo.findOne({ name: SettingsPreset.default_name });
         if ( !defaultSettingsPreset ) {
 
@@ -29,6 +48,8 @@ export async function initializeApp() {
             defaultSettingsPreset.updateOcrEngineSettings({ cpu_threads: os.cpus().length });
             await settingsPresetRepo.insert( defaultSettingsPreset );
         }
+
+        
 
         let defaultLanguage = await languageRepo.findOne({ name: 'japanese' });
         if ( !defaultLanguage ) {
