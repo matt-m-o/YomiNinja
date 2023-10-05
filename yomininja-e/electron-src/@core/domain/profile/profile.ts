420c23785a3e489be9fe2236a29e7ec73d822206
MatheusM8 <matheusmoura8@hotmail.com>
2023-10-04 22:11:58 -0300

    Added toJson method to Profile entity

--- yomininja-e/electron-src/@core/domain/profile/profile.ts
+++ yomininja-e/electron-src/@core/domain/profile/profile.ts
@@ -1,6 +1,6 @@
 import { randomUUID } from "crypto";
-import { SettingsPreset } from "../settings_preset/settings_preset";
-import { Language } from "../language/language";
+import { SettingsPreset, SettingsPresetJson } from "../settings_preset/settings_preset";
+import { Language, LanguageJson } from "../language/language";
 
 export interface ProfileProps {
     name: string;
@@ -65,4 +65,23 @@ export class Profile {
     
     protected set created_at( date: Date ){ this.props.created_at = date; }
     protected set updated_at( date: Date ){ this.props.updated_at = date; }
+
+    toJson(): ProfileJson {
+        return {
+            id: this.id,
+            ...this.props,
+            active_ocr_language: this.active_ocr_language.toJson(),
+            active_settings_preset: this.active_settings_preset.toJson(),
+        }
+    }
+}
+
+export interface ProfileJson extends Omit< 
+    ProfileProps,
+    'active_ocr_language' | 'active_settings_preset'
+> {
+    id: string;
+    name: string;
+    active_ocr_language: LanguageJson,
+    active_settings_preset: SettingsPresetJson,
 }
