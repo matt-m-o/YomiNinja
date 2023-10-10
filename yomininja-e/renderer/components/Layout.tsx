b2f2ace520667a32e9adede4b944501c6770250a
MatheusM8 <matheusmoura8@hotmail.com>
2023-10-10 16:00:33 -0300

    Changed app background color, matching the AppBar color

--- yomininja-e/renderer/components/Layout.tsx
+++ yomininja-e/renderer/components/Layout.tsx
@@ -194,7 +194,7 @@ export default function Layout( { contents }: LayoutProps) {
             backgroundColor: (theme) =>
               theme.palette.mode === 'light'
                 ? theme.palette.grey[100]
-                : theme.palette.grey[900],
+                : theme.palette.grey[1000],
             flexGrow: 1,          
             height: '100vh',
             overflow: 'auto',
