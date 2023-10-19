4e134a026b5c60a033e5a45b73781ea5b803ed84
MatheusM8 <matheusmoura8@hotmail.com>
2023-10-19 03:40:20 -0300

    Implemented DictionaryTag entity

---
+++ yomininja-e/electron-src/@core/domain/dictionary/dictionary_tag/dictionary_tag.spec.ts
@@ -0,0 +1,39 @@
+import { RawDictionaryTag, getRawDictionaryTags } from "../common/test/dictionary_tag_test_data";
+import { DictionaryTag, DictionaryTagCreationInput } from "./dictionary_tag";
+
+
+
+describe("DictionaryTag tests", () => {
+
+    let rawDictionaryTags: RawDictionaryTag[];
+
+    beforeEach( () => {
+        
+        rawDictionaryTags = getRawDictionaryTags();
+    });
+
+    it("should define a dictionary tag", () => {
+
+        const rawPnTag = rawDictionaryTags.find( rawTag => rawTag.name == 'pn' );
+        if ( !rawPnTag ) return;
+
+        const input: DictionaryTagCreationInput = {
+            ...rawPnTag,
+            dictionary_id: 'asdf'
+        };
+
+        const dictionaryTag = DictionaryTag.create( input );
+
+        expect( dictionaryTag.id ).
+            toStrictEqual(
+                input.dictionary_id + '/' + input.name
+            );
+        expect( dictionaryTag.dictionary_id ).toStrictEqual( input.dictionary_id );
+        expect( dictionaryTag.name ).toStrictEqual( input.name );
+        expect( dictionaryTag.content ).toStrictEqual( input.content );
+        expect( dictionaryTag.category ).toStrictEqual( input.category );
+        expect( dictionaryTag.order ).toStrictEqual( input.order );
+        expect( dictionaryTag.popularity_score ).toStrictEqual( input.popularity_score );
+    });
+
+});
---
+++ yomininja-e/electron-src/@core/domain/dictionary/dictionary_tag/dictionary_tag.ts
@@ -0,0 +1,53 @@
+import { DictionaryId } from "../dictionary";
+
+export interface DictionaryTagId extends String {};
+
+export type DictionaryTagConstructorProps = {
+    id?: string; // !change it to "number" later
+    dictionary_id: string;
+    name: string;
+    category: string;
+    content: string; // note
+    order: number; // for sorting
+    popularity_score: number;
+}
+
+export interface DictionaryTagCreationInput extends Omit< DictionaryTagConstructorProps, 'id' > {};
+
+// Entity based on Yomichan tag banks
+export class DictionaryTag {
+
+    id: DictionaryTagId; // !change it to "number" later
+    dictionary_id: DictionaryId;
+    name: string;
+    category: string;
+    content: string; // note
+    order: number; // for sorting
+    popularity_score: number;
+
+    protected constructor( props: DictionaryTagConstructorProps ) {
+
+        this.id = props?.id || DictionaryTag.generateId({
+            dictionary_id: props.dictionary_id,
+            tag_name: props.name
+        });
+
+        this.dictionary_id = props.dictionary_id;
+        this.name = props.name;
+        this.category = props.category;
+        this.content = props.content;
+        this.order = props.order;
+        this.popularity_score = props.popularity_score;
+    }
+
+    static create( input: DictionaryTagCreationInput ) {
+        return new DictionaryTag({
+            ...input,            
+        });
+    }
+
+    // ! might change to a hashing function
+    static generateId( input: { dictionary_id: string, tag_name: string } ): string {
+        return input.dictionary_id + '/' + input.tag_name;
+    }
+}
