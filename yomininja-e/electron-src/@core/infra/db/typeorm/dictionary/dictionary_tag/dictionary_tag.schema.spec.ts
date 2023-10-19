8f27e28e20206d968d1f51292e03949679077fb0
MatheusM8 <matheusmoura8@hotmail.com>
2023-10-19 15:45:51 -0300

    Implemented DictionaryTagTypeOrmSchema

---
+++ yomininja-e/electron-src/@core/infra/db/typeorm/dictionary/dictionary_tag/dictionary_tag.schema.spec.ts
@@ -0,0 +1,52 @@
+import { DataSource, FindOptionsWhere, Repository } from 'typeorm';
+import { DictionaryTag } from '../../../../../domain/dictionary/dictionary_tag/dictionary_tag';
+import { DictionaryTagTypeOrmSchema } from './dictionary_tag.schema';
+import { RawDictionaryTag, getRawDictionaryTags } from '../../../../../domain/dictionary/common/test/dictionary_tag_test_data';
+
+describe( 'DictionaryTag Entity Schema tests', () => {
+
+    let dataSource: DataSource;
+    let dictionaryTagTypeOrmRepo: Repository< DictionaryTag >;
+
+    let rawDictionaryTags: RawDictionaryTag[];
+
+    let rawPnTag: RawDictionaryTag | undefined;
+
+    beforeEach( async () => {
+        dataSource = new DataSource({
+            type: 'sqlite',
+            database: ':memory:',
+            synchronize: true,
+            logging: false,
+            entities: [ DictionaryTagTypeOrmSchema ],
+        });
+
+        await dataSource.initialize();
+
+        dictionaryTagTypeOrmRepo = dataSource.getRepository( DictionaryTag );
+
+        rawDictionaryTags = getRawDictionaryTags();
+
+        rawPnTag = rawDictionaryTags.find( item => item.name === 'pn' );        
+    });
+
+    it("should insert a tag", async () => {
+
+        if ( !rawPnTag ) return;
+
+        const dictionaryTag = DictionaryTag.create({
+            ...rawPnTag,
+            dictionary_id: 'zxcv'
+        });
+        
+        await dictionaryTagTypeOrmRepo.save(dictionaryTag);
+
+        const foundTag = await dictionaryTagTypeOrmRepo.findOneBy({ id: dictionaryTag.id });
+
+        expect( foundTag ).toBeDefined();
+        expect( foundTag?.id ).toStrictEqual( dictionaryTag.id );
+        expect( foundTag ).toStrictEqual( dictionaryTag );
+
+    });
+
+});
---
+++ yomininja-e/electron-src/@core/infra/db/typeorm/dictionary/dictionary_tag/dictionary_tag.schema.ts
@@ -0,0 +1,59 @@
+
+
+import { EntitySchema } from 'typeorm';
+import { DictionaryTag } from '../../../../../domain/dictionary/dictionary_tag/dictionary_tag';
+import { generateIndexName } from '../../common/schema_helpers';
+
+
+const name = 'dictionary_tag';
+export const DictionaryTagTypeOrmSchema = new EntitySchema< DictionaryTag >({
+
+    name,
+    target: DictionaryTag,
+
+    columns: {
+
+        id: {
+            type: String,
+            length: 40,
+            primary: true,
+        },
+        
+        dictionary_id: {
+            type: String,
+            length: 40,
+        },
+
+        name: {
+            type: String,
+            length: 100,
+        },
+
+        category: {
+            type: String,
+            length: 100,
+        },
+
+        content: {
+            type: String,
+            length: 100,
+        },
+
+        order: {
+            type: Number,
+        },
+
+        popularity_score: {
+            type: Number
+        }
+    },
+
+    indices: [
+
+        {
+            name: generateIndexName(name, 'dictionary_id'),
+            columns: ['dictionary_id'],
+        },
+
+    ]
+});
