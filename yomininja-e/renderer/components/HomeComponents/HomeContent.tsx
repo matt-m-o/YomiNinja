664c1808020a442e16a2565ed31c57d5c1bc5259
MatheusM8 <matheusmoura8@hotmail.com>
2023-11-02 01:39:44 -0300

    edited new features message

--- yomininja-e/renderer/components/HomeComponents/HomeContent.tsx
+++ yomininja-e/renderer/components/HomeComponents/HomeContent.tsx
@@ -123,7 +123,7 @@ export default function HomeContent() {
                     >
                         <li> Option to invert the captured image colors. Good for text on a dark background.</li>
                         <li> WebSocket for texthookers. Port: 6677.</li>
-                        <li> Option to change the Inference runtime. This might be helpful if you are experiencing issues with the OCR. </li>
+                        <li> Option to change the Inference runtime. This might be helpful if you are experiencing issues with OCR functionality. </li>
                     </ul>
 
                 </CardContent>
