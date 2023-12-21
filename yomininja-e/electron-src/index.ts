2be15b5b3d89aecc53833d2e62f022af57cf2799
MatheusM8 <matheusmoura8@hotmail.com>
2023-12-21 16:25:55 -0300

    removed unused imports

--- yomininja-e/electron-src/index.ts
+++ yomininja-e/electron-src/index.ts
@@ -1,21 +1,8 @@
 // Packages
-import { initializeApp } from './@core/infra/app_initialization';
 import { BrowserWindow, app, ipcMain, IpcMainEvent, IpcMainInvokeEvent, clipboard, globalShortcut } from 'electron';
-import isDev from 'electron-is-dev';
 import prepareNext from 'electron-next';
-import { PAGES_DIR } from './util/directories.util';
-import { ocrRecognitionController } from './ocr_recognition/ocr_recognition.index';
-import { settingsController } from './settings/settings.index';
 import './shared_handlers';
 import { uIOhook } from 'uiohook-napi';
-import { appInfoController } from './app_info/app_info.index';
-import { profileController } from './profile/profile.index';
-import { overlayController } from './overlay/overlay.index';
-import { mainController } from './main/main.index';
-import { dictionariesController } from './dictionaries/dictionaries.index';
-import { createDebuggingWindow } from './util/debugging/debugging.util';
-import { browserExtensionsController } from './extensions/extensions.index';
-import { ocrTemplatesController } from './ocr_templates/ocr_templates.index';
 import { appController } from './app/app.index';
 
 
