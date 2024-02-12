b1f3b2bc388c05e330c03b9a453fbff030f19ad5
MatheusM8 <matheusmoura8@hotmail.com>
2024-02-12 16:30:39 -0300

    added instructions to the Extensions screen

--- yomininja-e/renderer/components/Extensions/Extensions.tsx
+++ yomininja-e/renderer/components/Extensions/Extensions.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Card, CardContent, Container, Divider, Grid, Typography, styled } from "@mui/material";
-import { CSSProperties, useContext, useEffect, useState } from "react";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Card, CardContent, Container, Divider, Grid, SxProps, Theme, Typography, styled } from "@mui/material";
+import { CSSProperties, ReactNode, useContext, useEffect, useState } from "react";
 import { ExtensionsContext } from "../../context/extensions.provider";
 import Image  from 'next/image';
 import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
@@ -7,13 +7,26 @@ import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import ExtensionItem from "./ExtensionItem";
 import AlertDialog from "../common/AlertDialog";
 import { BrowserExtensionJson } from "../../../electron-src/@core/domain/browser_extension/browser_extension";
-
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const SectionDivider = styled( Divider )({
     marginTop: '30px',
     marginBottom: '30px',
 });
 
+const UL = styled( 'ul' )({
+    fontSize: '1rem',
+    lineHeight: 2,
+    marginTop: 0,
+    marginLeft: 5
+});
+
+const OL = styled( 'ol' )({
+    fontSize: '1rem',
+    lineHeight: 2,
+    marginTop: 0,
+    marginLeft: 5
+});
 
 export default function Extensions() {
 
@@ -82,6 +95,21 @@ export default function Extensions() {
         global.ipcRenderer.invoke( 'open_link', link );
     }
 
+    function CustomAccordion( props: { summary: any, children: ReactNode, sx?: SxProps<Theme> } ) {
+        return(
+            <Accordion sx={{ backgroundColor: '#202124' }}>
+                <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+                    <Typography fontSize={'1.1rem'}>
+                        {props.summary}
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails sx={{ pl: 1, ...props.sx }}>
+                    {props.children}
+                </AccordionDetails>
+            </Accordion>
+        );
+    }
+
     return (
         <Card variant="elevation" sx={{ borderRadius: 4, userSelect: 'none' }}>
 
@@ -144,22 +172,64 @@ export default function Extensions() {
 
                         </Box>
 
-                        <Typography fontSize='1rem' lineHeight={2} mt='56px'>
-                            Important notes:
-                        </Typography>                        
-                        <ul
-                            style={{
-                                fontSize: '1rem',                                
-                                lineHeight: 2,
-                                marginTop: 0,
-                                marginLeft: 5
-                            }}
-                        >
-                            <li> Not all Chrome extensions are currently supported. </li>
-                            <li> Compatibility may improve in the future as Electron development progresses. </li>
-                            <li> Download extensions using {crxExtractorLink} or from {chromeStatsLink} </li>
-                            <li> Clicking on the refresh button might resolve some extension-related problems. </li>
-                        </ul>
+                        <Box m={1} mt={5}>
+                            <CustomAccordion
+                                summary={
+                                    <Typography fontSize={'1.1rem'}>
+                                        Important notes
+                                    </Typography>
+                                }
+                            >
+                                <UL>
+                                    <li> Not all Chrome extensions are currently supported. </li>
+                                    <li> Compatibility may improve in the future as Electron development progresses. </li>
+                                    <li> Download extensions using {crxExtractorLink} or from {chromeStatsLink} </li>
+                                    <li> Clicking on the refresh button might resolve some extension-related problems. </li>
+                                </UL>
+                            </CustomAccordion>
+
+                            <CustomAccordion
+                                summary={
+                                    <Typography fontSize={'1.1rem'}>
+                                        How to open Yomitan/Yomichan options
+                                    </Typography>
+                                }
+                                sx={{ pl: 5 }}
+                            >
+                                To open Yomitan/Yomichan options:
+                                <OL>
+                                    <li> Right-click the Yomitan icon. </li>
+                                    <li> Click on "Options". </li>
+                                </OL>
+
+                                For a better experience, it's recommended to enable "Hide popup on cursor exit":
+
+                                <OL>
+                                    <li> Open Yomitan/Yomichan settings. </li>
+                                    <li> Go to the "Scanning" section. </li>
+                                    <li> Enable "Hide popup on cursor exit". </li>
+                                </OL>
+                            </CustomAccordion>
+
+                            <CustomAccordion
+                                summary={
+                                    <Typography fontSize={'1.1rem'}>
+                                        How to open JPDBReader options and set the API key
+                                    </Typography>
+                                }
+                                sx={{ }}
+                            >
+                                <OL>
+                                    <li> Make sure JPDB Reader is enabled. </li>
+                                    <li> Click on its icon (upper-right corner). </li>
+                                    <li> Right-click "Settings". </li>
+                                    <li> Click on "Open link in a new window". </li>
+                                    <li> Paste your JPDB.io API key into the "API Token" field and click on "Save". </li>
+                                </OL>
+
+                            </CustomAccordion>
+
+                        </Box>
 
                     </Box>
 
