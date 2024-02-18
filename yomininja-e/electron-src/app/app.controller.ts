5192da408789de6dccd14ef6bc8af0f7fdbfeb7b
MatheusM8 <matheusmoura8@hotmail.com>
2024-02-18 17:30:01 -0300

    increased app startup timeout

--- yomininja-e/electron-src/app/app.controller.ts
+++ yomininja-e/electron-src/app/app.controller.ts
@@ -64,7 +64,7 @@ export class AppController {
             startupTimer = setTimeout( () => {
                 console.log('Initialization took too long. Closing the app.');
                 app.quit();
-            }, 15_000 );
+            }, 30_000 );
         }
 
         this.mainWindow = await mainController.init();
