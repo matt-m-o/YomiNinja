89e9fee825e6dd66cf7fafce4a263b06b824d104
MatheusM8 <matheusmoura8@hotmail.com>
2024-03-29 18:37:15 -0300

    created VideoAnalyzerAdapter

---
+++ yomininja-e/electron-src/@core/application/adapters/video_analyzer.adapter.ts
@@ -0,0 +1,21 @@
+export type DetectMotion_Input = {
+    streamId: string;
+    videoFrame: Buffer;
+    clearPreviousFrames?: boolean;
+};
+
+export type DetectMotion_Output = {
+    motionPixelsCount: number; // Number of pixels (motion area)
+};
+
+
+export interface VideoAnalyzerAdapter {
+    status: VideoAnalyzerStatus;
+    detectMotion: ( input: DetectMotion_Input ) => Promise< DetectMotion_Output >;
+}
+
+export enum VideoAnalyzerStatus {
+    Enabled = "Enabled",
+    Disabled = "Disabled",
+    Processing = "Processing",
+}
