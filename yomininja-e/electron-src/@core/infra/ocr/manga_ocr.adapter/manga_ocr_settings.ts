493823f531fce526da28f5d976723c2c84651bbc
MatheusM8 <matheusmoura8@hotmail.com>
2024-06-02 10:25:41 -0300

    updated MangaOcrEngineSettings

--- yomininja-e/electron-src/@core/infra/ocr/manga_ocr.adapter/manga_ocr_settings.ts
+++ yomininja-e/electron-src/@core/infra/ocr/manga_ocr.adapter/manga_ocr_settings.ts
@@ -1,13 +1,14 @@
 import { OcrEngineSettings } from "../../../domain/settings_preset/settings_preset";
 // import { applyCpuHotfix } from "./hotfix/hardware_compatibility_hotfix";
+import os from 'os';
 
 // For preventing 'electron-is-dev' issues when testing with jest
 export const mangaOcrAdapterName = 'MangaOcrAdapter';
 
 export interface MangaOcrEngineSettings extends OcrEngineSettings {
-    // max_image_width: number;
-    // cpu_threads: number;
-    // inference_runtime: string;
+    text_detector: string | 'PaddleTextDetector' | 'ComicTextDetector';
+    cpu_threads: number;
+    inference_runtime: string; // PyTorch | ONNX
     // det_db_thresh: number; // Only pixels with a score greater than this threshold will be considered as text pixels
     // det_db_box_thresh: number; // When the average score of all pixels is greater than the threshold, the result will be considered as a text area
     // det_db_unclip_ratio: number; // Expansion factor of the Vatti clipping algorithm, which is used to expand the text area
@@ -23,9 +24,10 @@ export function getMangaOcrDefaultSettings() {
         hotkey: 'Alt+A',
         image_scaling_factor: 1,
         invert_colors: false,
+        text_detector: 'ComicTextDetector',
+        cpu_threads: os.cpus().length,
+        inference_runtime: 'PyTorch',
         // max_image_width: 1600,
-        // cpu_threads: os.cpus().length,
-        // inference_runtime: 'Open_VINO',
         // det_db_thresh: 0.3,
         // det_db_box_thresh: 0.6,
         // det_db_unclip_ratio: 1.6,
