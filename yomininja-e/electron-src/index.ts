b3092b7d1b1efe736da7ce9f5a5521a3e61b212e
MatheusM8 <matheusmoura8@hotmail.com>
2024-06-25 12:19:15 -0300

    added app launch config

--- yomininja-e/electron-src/index.ts
+++ yomininja-e/electron-src/index.ts
@@ -7,15 +7,26 @@ import prepareNext from 'electron-next';
 import './shared_handlers';
 import { uIOhook } from 'uiohook-napi';
 import { appController } from './app/app.index';
+import { USER_DATA_DIR } from './util/directories.util';
+import fs from 'fs';
+import path from 'path';
 const isMacOS = process.platform === 'darwin';
 
+
+preInitialization();
 // Prevent multiple instances
 const gotTheLock = app.requestSingleInstanceLock();
+// app.disableHardwareAcceleration();
 
 if (!gotTheLock)
   app.quit();
+
 else {
   app.on('ready', async () => {
+
+    if ( process.platform === 'win32' ) {
+      app.setAppUserModelId(`com.yomininja.${app.getName()}`);
+    }
   
     // Prepare the renderer once the app is ready
     await prepareNext('./renderer');
@@ -48,4 +59,41 @@ function startUIOhook(){
     return;
 
   uIOhook.start();
+}
+
+function preInitialization() {
+
+  const filePath = path.join(USER_DATA_DIR, 'launch_config.json');
+  const fileExists = fs.existsSync(filePath);
+
+  let fileData: string | undefined;
+  
+  if (fileExists) {
+    fileData = fs.readFileSync(
+      path.join(USER_DATA_DIR, 'launch_config.json'),
+      'utf8'
+    );
+  }
+
+  let launchConfig;
+
+  if ( fileData )
+    launchConfig = JSON.parse(fileData);
+
+  console.log({ launchConfig });
+
+  if ( !launchConfig ) {
+    launchConfig = {
+      hardware_acceleration: true
+    };
+    fs.writeFileSync(
+      filePath,
+      JSON.stringify( launchConfig, null, '\t' )
+    );
+  }
+
+  if ( !launchConfig.hardware_acceleration === false ) {
+    app.disableHardwareAcceleration();
+  }
+  
 }
