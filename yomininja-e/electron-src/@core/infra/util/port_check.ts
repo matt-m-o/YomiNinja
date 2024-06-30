6c9b9a7b637edaf85388064b075482fbcb2c1b75
MatheusM8 <matheusmoura8@hotmail.com>
2024-06-30 15:21:02 -0300

    improved port handling

--- yomininja-e/electron-src/@core/infra/util/port_check.ts
+++ yomininja-e/electron-src/@core/infra/util/port_check.ts
@@ -2,31 +2,51 @@ import net from 'net';
 
 
 export async function isPortInUse( port: number, host = '0.0.0.0' ): Promise< boolean > {
-        return new Promise(( resolve, reject ) => {
-      
-            const client = new net.Socket();
-        
-            client.connect( port, host, () => {
-                resolve(true);
-                client.end();
-                client.destroy();
-            });
-        
-            client.on('error', (err) => {
-                resolve(false);
-                client.end();
-                client.destroy();
-            });
+    return new Promise(( resolve, reject ) => {
+    
+        const client = new net.Socket();
+    
+        client.connect( port, host, () => {
+            resolve(true);
+            client.end();
+            client.destroy();
         });
-    }
+    
+        client.on('error', (err) => {
+            resolve(false);
+            client.end();
+            client.destroy();
+        });
+    });
+}
+
+export async function isPortAvailable( port: number ): Promise< boolean > {
+    return new Promise( (resolve, reject) => {
+
+        const server = net.createServer((socket) => {
+            socket.on( 'connect', () => socket.end() );
+        });
+    
+        server.on( 'error', ( e ) => {
+            console.error(e);
+            server.close();
+            resolve(false);
+        });
+      
+        server.listen( port, () => {
+            server.close();
+            resolve(true);
+        });
+    });
+}
 
 export async function getNextPortAvailable( port: number, host = '0.0.0.0' ): Promise< number | undefined > {
 
     while( port < 65_535 ) {
 
-        const inUse = await isPortInUse( port, host );
+        const isAvailable = await isPortAvailable( port );
 
-        if ( !inUse ) return port;
+        if ( isAvailable ) return port;
 
         port++;
     }
