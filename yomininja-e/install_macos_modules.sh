388703e1739e8d610fffd9ecce4c59bae4997455
MatheusM8 <matheusmoura8@hotmail.com>
2024-07-09 14:47:08 -0300

    added script to install macOS specific dependencies

---
+++ yomininja-e/install_macos_modules.sh
@@ -0,0 +1,8 @@
+npm install --cpu=x64 --os=darwin --include=optional --force sharp
+npm install --cpu=arm64 --os=darwin --include=optional --force sharp
+
+npm install --force --include=optional @img/sharp-darwin-x64
+npm install --force --include=optional @img/sharp-darwin-arm64
+
+npm install --force --include=optional @img/sharp-libvips-darwin-x64
+npm install --force --include=optional @img/sharp-libvips-darwin-arm64
