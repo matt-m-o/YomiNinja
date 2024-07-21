fe80c6c03dd4715da8a4beca23c70b4d21e27279
MatheusM8 <matheusmoura8@hotmail.com>
2024-07-21 15:37:24 -0300

    updated py_ocr_service build copy script

--- copy_py_ocr_service_build.bat
+++ copy_py_ocr_service_build.bat
@@ -1,1 +1,2 @@
-xcopy .\\ocr_services\\py_ocr_service\\dist\\py_ocr_service\\ .\\bin\\win32\\py_ocr_service\\service\\ /e
+xcopy .\\ocr_services\\py_ocr_service\\models\\py_ocr_service\\ .\\bin\\win32\\py_ocr_service\\service\\ /e
+xcopy .\\ocr_services\\py_ocr_service\\models\\ .\\bin\\win32\\py_ocr_service\\models\\ /e
