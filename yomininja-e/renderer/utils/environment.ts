224615096c6032fe6fd352454fe9573cba77b842
MatheusM8 <matheusmoura8@hotmail.com>
2024-08-10 10:33:27 -0300

    implemented onDisplayModeChange utility

--- yomininja-e/renderer/utils/environment.ts
+++ yomininja-e/renderer/utils/environment.ts
@@ -10,3 +10,43 @@ export function isFullscreenWindow( window: Window ): boolean {
 
     return window.matchMedia("(display-mode: fullscreen").matches;
 }
+
+export function onDisplayModeChange(
+    window: Window,
+    callBack: ( displayMode: string ) => void
+): () => void {
+
+    const modes = [
+        'browser',
+        'fullscreen',
+        'window-controls-overlay',
+        'standalone'
+    ];
+
+
+    const listener = ( e: MediaQueryListEvent ) => {
+        let displayMode = 'browser';
+
+        displayMode = modes.find( mode =>
+            window.matchMedia(`(display-mode: ${mode})`).matches
+        ) || displayMode;
+
+        // console.log(e)
+        callBack( displayMode );
+        console.log('display-mode: '+ displayMode);
+    }
+
+    window.matchMedia( '(display-mode: browser)' )
+        .addEventListener( 'change', listener );
+
+    window.matchMedia( '(display-mode: standalone)' )
+        .addEventListener( 'change', listener );
+
+    return () => {
+        window.matchMedia( '(display-mode: browser)' )
+            .removeEventListener( 'change', listener );
+
+        window.matchMedia( '(display-mode: standalone)' )
+            .removeEventListener( 'change', listener );
+    };
+}
