5a9fbff37dc7077dd50a3bbbdb47c79ff6962e8e
MatheusM8 <matheusmoura8@hotmail.com>
2024-08-23 18:59:34 -0300

    added missing provider

--- yomininja-e/renderer/pages/ocr-overlay-browser.tsx
+++ yomininja-e/renderer/pages/ocr-overlay-browser.tsx
@@ -1,14 +1,17 @@
 import { OcrResultProvider } from '../context/ocr_result.provider';
 import OcrOverlayForBrowsers from '../components/OcrOverlay/OcrOverlayForBrowsers';
 import { AppInstallationProvider } from '../context/app_installation.provider';
+import { SettingsProvider } from '../context/settings.provider';
 
 export default function OcrOverlayBrowserPage() {
 
     return (
-        <AppInstallationProvider>
-            <OcrResultProvider>
-                <OcrOverlayForBrowsers/>
-            </OcrResultProvider>
-        </AppInstallationProvider>
+        <SettingsProvider>
+            <AppInstallationProvider>
+                <OcrResultProvider>
+                    <OcrOverlayForBrowsers/>
+                </OcrResultProvider>
+            </AppInstallationProvider>
+        </SettingsProvider>
     );
 }
