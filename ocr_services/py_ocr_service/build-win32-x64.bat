db124eb9e80d7c59f548d5b04f2a868a86f68c1d
MatheusM8 <matheusmoura8@hotmail.com>
2024-11-03 19:06:41 -0300

    update python path

--- ocr_services/py_ocr_service/build-win32-x64.bat
+++ ocr_services/py_ocr_service/build-win32-x64.bat
@@ -1,4 +1,4 @@
 rmdir .\\dist\\py_ocr_service\\ /S /Q
-xcopy .\\.python\\win32\\x64\\ .\\dist\\py_ocr_service\\python\\ /e /I /Y
+xcopy .\\python\\win32\\x64\\ .\\dist\\py_ocr_service\\python\\ /e /I /Y
 xcopy .\\models\\ .\\dist\\py_ocr_service\\models\\ /e /Y
 xcopy .\\src\\ .\\dist\\py_ocr_service\\src\\ /e /I /Y 
