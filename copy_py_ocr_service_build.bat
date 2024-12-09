37b0b4eba403f72241ae1d5a46bfa708675b44ba
MatheusM8 <matheusmoura8@hotmail.com>
2024-12-09 00:07:47 -0300

    update PyOCRService bin directory

--- copy_py_ocr_service_build.bat
+++ copy_py_ocr_service_build.bat
@@ -1,1 +1,3 @@
-xcopy .\\ocr_services\\py_ocr_service\\dist\\py_ocr_service\\ .\\bin\\win32\\py_ocr_service\\service\\ /e
+rmdir .\\bin\\win32\\py_ocr_service\\ /S /Q
+xcopy .\\ocr_services\\py_ocr_service\\dist\\py_ocr_service\\ .\\bin\\win32\\py_ocr_service\\ /e
+pause
