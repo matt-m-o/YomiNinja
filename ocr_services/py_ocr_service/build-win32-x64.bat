6d6e1cab6750ed7d772879c606de6decf9122b76
MatheusM8 <matheusmoura8@hotmail.com>
2024-12-15 22:24:40 -0300

    Remove MangaOCR model from bundle

--- ocr_services/py_ocr_service/build-win32-x64.bat
+++ ocr_services/py_ocr_service/build-win32-x64.bat
@@ -1,5 +1,5 @@
 rmdir .\\dist\\py_ocr_service\\ /S /Q
 xcopy .\\python\\win32\\x64\\ .\\dist\\py_ocr_service\\python\\ /e /I /Y
-xcopy .\\models\\ .\\dist\\py_ocr_service\\models\\ /e /Y
+xcopy .\\models\\comic_text_detector\\ .\\dist\\py_ocr_service\\models\\comic_text_detector\\ /e /Y
 xcopy .\\src\\ .\\dist\\py_ocr_service\\src\\ /e /I /Y 
 xcopy .\\run.bat .\\dist\\py_ocr_service\\src\\run.bat /Y
