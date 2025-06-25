131e2afc8a6c516c16a1073fc62b85b04601a65a
MatheusM8 <matheusmoura8@hotmail.com>
2025-06-24 22:20:22 -0300

    Update python path

--- ocr_services/py_ocr_service/build-win32-x64.bat
+++ ocr_services/py_ocr_service/build-win32-x64.bat
@@ -1,5 +1,7 @@
-rmdir .\\dist\\py_ocr_service\\ /S /Q
-xcopy .\\python\\win32\\x64\\ .\\dist\\py_ocr_service\\python\\ /e /I /Y
-xcopy .\\models\\comic_text_detector\\ .\\dist\\py_ocr_service\\models\\comic_text_detector\\ /e /Y
-xcopy .\\src\\ .\\dist\\py_ocr_service\\src\\ /e /I /Y 
-xcopy .\\run.bat .\\dist\\py_ocr_service\\src\\run.bat /Y
+rmdir .\\dist\\win32\\x64\\py_ocr_service\\ /S /Q
+
+xcopy .\\python\\win32\\x64\\ .\\dist\\win32\\x64\\py_ocr_service\\python\\ /e /I /Y
+
+xcopy .\\models\\comic_text_detector\\ .\\dist\\win32\\x64\\py_ocr_service\\models\\comic_text_detector\\ /e /Y
+xcopy .\\src\\ .\\dist\\win32\\x64\\py_ocr_service\\src\\ /e /I /Y 
+xcopy .\\run.bat .\\dist\\win32\\x64\\py_ocr_service\\src\\run.bat /Y
--- ocr_services/py_ocr_service/install_requirements.bat
+++ ocr_services/py_ocr_service/install_requirements.bat
@@ -1,7 +1,7 @@
-IF EXIST ".\\.python\\win32\\x64\\python.exe" (
+IF EXIST ".\\python\\win32\\x64\\python.exe" (
     .\\python\\win32\\x64\\python.exe -m pip install -r requirements.txt
 )
 
-IF EXIST ".\\.python\\win32\\arm64\\python.exe" ( 
+IF EXIST ".\\python\\win32\\arm64\\python.exe" ( 
     .\\python\\win32\\arm64\\python.exe -m pip install -r requirements.txt
 )
